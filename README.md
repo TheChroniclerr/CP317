dfe96fb22e975b8471862c4a24e1bd3ef1091ce8
TheChroniclerr <hubert_bao@hotmail.com>
2025-10-27 19:21:25 -0400

    init

---
+++ README.md
@@ -0,0 +1,31 @@
+## Setup
+Frontend - React\
+Backend - Python 
+
+## Configure React-Vite client
+```powershell
+cd client
+npm install
+```
+
+## Configure Python-Flask server
+```powershell
+cd server
+python -m venv venv
+venv\Scripts\activate
+pip install -r requirements.txt
+```
+
+## Run Vite client
+```powershell
+cd client
+npm run dev
+```
+
+## Run Flask server
+```powershell
+cd server
+venv\Scripts\activate
+python src/main.py
+```
+
---
+++ server/src/main.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+cors = CORS(app, origins='http://localhost:5173')
+
+@app.route('/api/data', methods=['GET'])
+def users():
+    return jsonify(
+        {
+            "users": [
+                "Alice",
+                "Bob",
+                "Charlie"
+            ]
+        }
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
+
+# def get_data():
+#     sample_data = {
+#         "message": "Hello, World!",
+#         "status": "success"
+#     }
+#     return jsonify(sample_data)
+
+# if __name__ == '__main__':
+#     app.run(host='', port=5000)
