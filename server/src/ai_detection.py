5ac08da9294c90dd1203c82c6132e77546510104
vladimir sosa <vladimirsosa@vladimirs-MacBook-Air.local>
2025-10-28 22:05:56 -0400

    Update Ai detection

--- server/src/ai_detection.py
+++ server/src/ai_detection.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 import cv2
-'''
+
 LABEL_MAP = {
     'bottle': ['milk', 'orange juice'],
     'apple': ['apple'],
@@ -8,64 +8,7 @@ LABEL_MAP = {
     'banana': ['banana'],
     'refrigerator': [],  
 }
-'''
-LABEL_MAP = {
-    # Bottles & Drinks
-    'bottle': ['milk', 'orange juice', 'apple juice', 'water', 'soda', 'yogurt drink', 'smoothie', 'wine', 'beer'],
-    'can': ['soda', 'beer', 'energy drink', 'juice can'],
 
-    # Fruits (YOLO often confuses colors/shapes)
-    'apple': ['apple', 'green apple', 'red apple'],
-    'orange': ['orange', 'mandarin', 'tangerine'],
-    'banana': ['banana'],
-    'grape': ['grapes', 'red grapes', 'green grapes'],
-    'strawberry': ['strawberry'],
-    'blueberry': ['blueberry'],
-    'kiwi': ['kiwi'],
-    'melon': ['watermelon', 'cantaloupe', 'honeydew'],
-    'pear': ['pear'],
-    'pineapple': ['pineapple'],
-    'mango': ['mango'],
-
-    # Vegetables (YOLO often detects bowl/cup instead of vegetable)
-    'tomato': ['tomato', 'cherry tomato'],
-    'lettuce': ['lettuce', 'romaine', 'iceberg'],
-    'cucumber': ['cucumber'],
-    'carrot': ['carrot'],
-    'bell pepper': ['red bell pepper', 'green bell pepper', 'yellow bell pepper'],
-    'onion': ['onion', 'red onion', 'green onion'],
-    'garlic': ['garlic'],
-    'potato': ['potato', 'sweet potato'],
-    'broccoli': ['broccoli'],
-    'cauliflower': ['cauliflower'],
-    'spinach': ['spinach'],
-    'cabbage': ['cabbage', 'red cabbage'],
-
-    # Dairy & Eggs
-    'cheese': ['cheese', 'cheddar', 'mozzarella', 'parmesan', 'feta'],
-    'egg': ['egg', 'eggs'],
-    'butter': ['butter', 'margarine'],
-    'yogurt': ['yogurt', 'greek yogurt', 'plain yogurt'],
-
-    # Meat & Fish
-    'meat': ['chicken', 'beef', 'pork', 'bacon', 'ham', 'turkey'],
-    'fish': ['salmon', 'tuna', 'cod', 'shrimp'],
-
-    # Snacks & Misc
-    'bread': ['bread', 'baguette', 'whole wheat bread'],
-    'chips': ['chips', 'tortilla chips'],
-    'nuts': ['almonds', 'cashews', 'peanuts', 'mixed nuts'],
-    'bowl': ['salad', 'fruit mix', 'veggie mix', 'cheese'],
-    'cup': ['coffee', 'tea', 'milk'],
-
-    # Ignore irrelevant YOLO labels
-    'refrigerator': [],
-    'sink': [],
-    'knife': [],
-    'spoon': [],
-    'fork': [],
-    'bowl': ['lettuce', 'tomato', 'cheese'], # sometimes YOLO labels vegetables as bowl
-}
 
 
 MODEL_PATH = "yolov8n.pt"
