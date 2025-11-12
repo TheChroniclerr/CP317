5fcdcb3428742cf05cb86e6824288e825355c218
TheChroniclerr <hubert_bao@hotmail.com>
2025-11-11 22:17:08 -0500

    BREAKING CHANGE: Overwrite main with add-angular-project content
    - Reimplement frontend using Angular

---
+++ client/src/app/pages/shopping-list/shopping-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  imports: [],
+  templateUrl: './shopping-list.html',
+  styleUrls: ['./shopping-list.css'],
+})
+export class ShoppingListComponent {
+
+}
